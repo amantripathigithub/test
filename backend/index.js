b00ab53a6a2da2f5efee8d650c3beeafbafc6de6
amantripathigithub <at425020@gmail.com>
2023-03-23 22:25:53 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 
 var express = require("express");
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const path = require("path");
-
+const dotenv = require('dotenv');
 var app = express();
+dotenv.config({ path: './config.env' });
+require('./db/connection');
 
 app.set("view engine", "ejs");
+//const register = require("./model/patient");
+//app.use(require('./router/auth'));
+//const User = require('./model/patient');
+// for hotel schema
+//const Hotel = require('./model/contributor');
 
 
 app.get("/", (req, res) => {
@@ -13,6 +21,16 @@ app.get("/", (req, res) => {
     res.render(path.join(__dirname, "../frontend", "/contributor"));
 });
 
+
+app.get("/logincon", (req, res) => {
+    app.use(express.static("../frontend"));
+    res.render(path.join(__dirname, "../frontend", "/contributor-login"));
+});
+
+app.get("/signupcon", (req, res) => {
+    app.use(express.static("../frontend"));
+    res.render(path.join(__dirname, "../frontend", "/contributor-signup"));
+});
 app.listen(3000, () => {
     console.log("Server listening on port " + 3000);
 });
